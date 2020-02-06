f0c3e19ce001eb8cb1f42d3a79073f87705f1872
daquinoaldo <daquino.aldo@gmail.com>
2020-02-06 19:16:09 +0100

    Update phi.sh after installing cmake on phi

--- phi.sh
+++ phi.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env bash
 
-PHI_PATH=skyline
 PHI_USER=daquino-spm19
+PHI_PATH=skyline
 
 sep="\n====================================\n"
-printf "${sep}Running rsync: remote path ~/${PHI_PATH}${sep}"
 
-rsync -arzP --delete --filter=':- .gitignore' --exclude .gitignore --exclude phi.sh \
-    ./ ${PHI_USER}@C6320p-2.itc.unipi.it:~/${PHI_PATH}
+printf "${sep}Running rsync: remote path ~/${PHI_PATH}${sep}"
+rsync -arzP --delete --filter=':- .gitignore' ./ ${PHI_USER}@C6320p-2.itc.unipi.it:~/${PHI_PATH}
 
 printf "${sep}Opening a remote shell on the server.${sep}"
-
-ssh -t ${PHI_USER}@C6320p-2.itc.unipi.it \
-  "cd ${PHI_PATH} ; \
-  cmake -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ . ; \
-  make ; \
-  bash"
+ssh -t ${PHI_USER}@C6320p-2.itc.unipi.it "cd ${PHI_PATH} ; make ; bash"
